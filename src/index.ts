20a7493ba35902c6931a43f919f2b6eb7ca5f494
Admin <rysenka2004@gmail.com>
2024-09-26 17:31:34 +0300

    homework 3

--- src/index.ts
+++ src/index.ts
@@ -1,204 +1,74 @@
-type LecturerType = {
-    name: string;
-    surname: string;
-    position: string;
-    company: string;
-    experience: number;
-    courses: number;
-    contacts: {
-        email: string;
-        phone: number;
+abstract class Figure{
+    public abstract readonly COLOR: string;
+    public abstract readonly NAME: string;
+
+    public abstract calculateArea(): number;
+}
+
+class Circle extends Figure{
+    constructor(public radius: number,
+                public readonly COLOR: string,
+                public readonly NAME: string) {
+        super();
+    }
+
+    public override calculateArea(): number{
+            return Math.PI * Math.pow(this.radius, 2);
     }
 }
 
-class School {
-    // implement 'add area', 'remove area', 'add lecturer', and 'remove lecturer' methods
-
-    _areas: Area[] = [];
-    _lecturers: LecturerType[] = []; // Name, surname, position, company, experience, courses, contacts
-
-    addArea(area: Area): void{
-        this._areas.push(area);
+class Rectangle extends Figure{
+    constructor(public height: number,
+                public width: number,
+                public readonly COLOR: string,
+                public readonly NAME: string){
+        super();
     }
 
-    removeArea(area: Area): Area[]{
-        return this._areas.filter((x:Area) => x !== area);
+    public override calculateArea(): number {
+        return this.height * this.width;
     }
 
-    addLecturer(lecturer: LecturerType): void{
-        this._lecturers.push(lecturer);
-    }
-
-    removeLecturer(lecturer: LecturerType): LecturerType[]{
-        return this._lecturers.filter((x: LecturerType) => x !== lecturer);
-    }
-
-    get areas(): Area[] {
-        return this._areas;
-    }
-
-    get lecturers(): LecturerType[] {
-        return this._lecturers;
+    public print(): void{
+        console.log(`Formula: height * width = ${this.height} * ${this.width} = ${this.calculateArea()}`);
     }
 }
 
-class Area {
-    // implement getters for fields and 'add/remove level' methods
-    _levels: Level[] = [];
-    _name: string;
-
-    get levels(): Level[] {
-        return this._levels;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    addLevel(level: Level): void {
-        this._levels.push(level);
-    }
-
-    removeLevel(level: Level): Level[]{
-        return this._levels.filter((x: Level) => x !== level);
-    }
-
-    constructor(name: string) {
-        this._name = name;
-        this._levels = [];
+class Square extends Rectangle{
+    constructor(side: number,
+                public readonly COLOR: string,
+                public readonly NAME: string){
+        super(side, side, COLOR, NAME);
     }
 }
 
-class Level {
-    // implement getters for fields and 'add/remove group' methods
-
-    _groups: Group[] = [];
-    _name: string;
-    _description: string;
-
-    get groups(): Group[]{
-        return this._groups;
+class Triangle extends Figure{
+    constructor(public base: number,
+                public perpendicular_height: number,
+                public readonly COLOR: string,
+                public readonly NAME: string) {
+        super();
     }
 
-    get name(): string{
-        return this._name;
-    }
-
-    get description(): string{
-        return this._description;
-    }
-
-    addGroup(group: Group): void{
-        this._groups.push(group);
-    }
-
-    removeGroup(group: Group): Group[]{
-        return this._groups.filter((x: Group) => x !== group);
-    }
-
-    constructor(name: string, description: string) {
-        this._name = name;
-        this._description = description;
-        this._groups = [];
+    public override calculateArea(): number {
+        return (this.base * this.perpendicular_height) / 2;
     }
 }
 
-class Group {
-    // implement getters for fields and 'add/remove student' and 'set status' methods
+let circle:Circle = new Circle(12, "Red", "Circle");
+console.log(circle.calculateArea());
+//circle.COLOR = "Black"; (error)
 
-    _area: Area;
-    _status: string;
-    _students: Student[] = []; // Modify the array so that it has a valid toSorted method*
-    _directionName: string;
-    _levelName: string;
+let rectangle:Rectangle = new Rectangle(12, 10, "Green", "Rectangle");
+rectangle.print();
 
-    get area(): Area{
-        return this._area;
-    }
+let square:Square = new Square(12, "Yellow", "Square");
+square.print();
+console.log(`${square.COLOR} and ${square.NAME}`);
 
-    get status(): string{
-        return this._status;
-    }
+let rectSquare:Square = new Rectangle(12, 12, "Orange", "RectSquare");
+rectSquare.print();
+console.log(`${rectSquare.COLOR} and ${rectSquare.NAME}`);
 
-    get students(): Student[]{
-        return this._students;
-    }
-
-    get directionName(): string{
-        return this._directionName;
-    }
-
-    get levelName(): string{
-        return this._levelName;
-    }
-
-    constructor(directionName: string, levelName: string) {
-        this._directionName = directionName;
-        this._levelName = levelName;
-    }
-
-    addStudent(student: Student): void{
-        this._students.push(student);
-    }
-
-    removeStudent(student: Student): Student[]{
-        return this._students.filter((x: Student) => x!== student)
-    }
-
-    set status(status: string) {
-        this._status = status;
-    }
-
-    showPerformance(): Student[] {
-        return this._students.toSorted((a: Student, b: Student) => b.getPerformanceRating() - a.getPerformanceRating());
-    }
-}
-
-class Student {
-    // implement 'set grade' and 'set visit' methods
-
-    _firstName: string;
-    _lastName: string;
-    _birthYear: number;
-    _grades: {workName: string, mark: number}[] = []; // workName: mark
-    _visits: {lesson: number, present: boolean}[] = []; // lesson: present
-
-    constructor(firstName: string, lastName: string, birthYear: number) {
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._birthYear = birthYear;
-        this._grades = [];
-        this._visits = [];
-    }
-
-    get fullName(): string {
-        return `${this._lastName} ${this._firstName}`;
-    }
-
-    set fullName(value: string) {
-        [this._lastName, this._firstName] = value.split(' ');
-    }
-
-    get age(): number {
-        return new Date().getFullYear() - this._birthYear;
-    }
-
-    set grades(grade: {workName: string, mark: number}){
-        this._grades.push(grade);
-    }
-
-    set visits(visit: {lesson: number, present: boolean}){
-        this._visits.push(visit);
-    }
-
-    getPerformanceRating(): number {
-        const gradeValues: {workName: string, mark: number}[] = Object.values(this._grades);
-
-        if (!gradeValues.length) return 0;
-
-        const averageGrade: number = gradeValues.reduce((sum: number, grade: {workName: string, mark: number}) => sum + grade.mark, 0) / gradeValues.length;
-        const attendancePercentage: number = (this._visits.filter(present => present.present).length / this._visits.length) * 100;
-
-        return (averageGrade + attendancePercentage) / 2;
-    }
-}
+let triangle:Triangle = new Triangle(12, 19, "Pink", "Triangle");
+console.log(triangle.calculateArea());
