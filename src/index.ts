4996a753b404cf66d01a882134e1a545a5d0e9b3
Admin <rysenka2004@gmail.com>
2024-11-04 19:27:10 +0200

    homework 7

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,128 @@
-//1) Вам потрібно створити умовний тип, що служить для встановлення типу, що повертається з функції. Як параметр типу повинен обов'язково виступати функціональний тип.
-type FunctionReturnType<T> = T extends (...args: any[]) => infer U ? U : never;
-
-function str(): string{
-    return "Hello"
+abstract class FilteringItems{
+    abstract searchItems(query: string): TodoItem[];
+    abstract sortByStatus(): TodoItem[];
+    abstract sortByCreationTime(): TodoItem[];
 }
 
-let a: FunctionReturnType<typeof str>
+class TodoItem {
+    private static idCounter = 1;
+    public id: number;
+    public title: string;
+    public content: string;
+    public createdAt: Date;
+    public updatedAt: Date;
+    public isCompleted: boolean;
+    public requiresConfirmation: boolean;
 
-//2) Вам потрібно створити умовний тип, який приймає функціональний тип з одним параметром (або задовільним) та повертає кортеж, де перше значення - це тип, що функція повертає, а другий - тип її параметру.
-type TrupleType<T> = T extends (param: infer U) => infer R ? [R, U] : never;
+    constructor(title: string, content: string, requiresConfirmation = false) {
+        if (!title || !content) {
+            throw new Error("Title and content cannot be empty.");
+        }
+        this.id = TodoItem.idCounter++;
+        this.title = title;
+        this.content = content;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.isCompleted = false;
+        this.requiresConfirmation = requiresConfirmation;
+    }
 
-function numstr(a:number): string{
-    return "Hi"
+    update(content: string): void {
+        if (this.requiresConfirmation) {
+            const confirmation = confirm("Editing cannot be reversed. Are you sure about editing?");
+            if (!confirmation) return;
+        }
+        this.content = content;
+        this.updatedAt = new Date();
+    }
+
+    markAsCompleted(): void {
+        this.isCompleted = true;
+        this.updatedAt = new Date();
+    }
 }
 
-let b: TrupleType<typeof numstr>
+class TodoList extends FilteringItems{
+    private items: TodoItem[] = [];
+
+    addItem(title: string, content: string, requiresConfirmation = false): TodoItem {
+        const newItem = new TodoItem(title, content, requiresConfirmation);
+        this.items.push(newItem);
+        return newItem;
+    }
+
+    removeItem(id: number): void {
+        this.items = this.items.filter(item => item.id !== id);
+    }
+
+    editItem(id: number, newContent: string): void {
+        const item = this.getItemById(id);
+        if (item) {
+            item.update(newContent);
+        }
+    }
+
+    getItemById(id: number): TodoItem | undefined {
+        return this.items.find(item => item.id === id);
+    }
+
+    getAllItems(): TodoItem[] {
+        return this.items;
+    }
+
+    getTotalCount(): number {
+        return this.items.length;
+    }
+
+    getIncompleteCount(): number {
+        return this.items.filter(item => !item.isCompleted).length;
+    }
+
+    searchItems(query: string): TodoItem[] {
+        return this.items.filter(item =>
+            item.title.includes(query) || item.content.includes(query)
+        );
+    }
+
+    sortByStatus(): TodoItem[] {
+        return this.items.sort((a, b) => Number(a.isCompleted) - Number(b.isCompleted));
+    }
+
+    sortByCreationTime(): TodoItem[] {
+        return this.items.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
+    }
+}
+
+const todoList = new TodoList();
+todoList.addItem("Зробити завдання", "Розробити додаток TODO List", true);
+todoList.addItem("Сходити в магазин", "Купити молоко, цукор, хліб");
+todoList.addItem("Прочитати книгу", "Прочитати два розділи Хіба ревуть воли як ясла повні");
+
+const item = todoList.getItemById(1);
+if (item) {
+    item.markAsCompleted();
+}
+
+console.log("Total items:", todoList.getTotalCount());
+
+console.log("All:", todoList.getAllItems());
+
+todoList.removeItem(1);
+
+console.log("Total items after removing:", todoList.getTotalCount());
+
+console.log("All after removing:", todoList.getAllItems());
+
+todoList.editItem(2, "ITEM AFTER EDITING");
+
+console.log("Item after editing: ", todoList.getItemById(2));
+
+console.log("Incomplete items:", todoList.getIncompleteCount());
+
+console.log("Search results:", todoList.searchItems("магазин"));
+
+console.log("Sorted by status:", todoList.sortByStatus());
+
+console.log("Sorted by creation time:", todoList.sortByCreationTime());
+
+
